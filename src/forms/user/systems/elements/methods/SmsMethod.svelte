d36b2b8b4dcba4e042bb55e29fcb8fc84da144e1
seihoukei <a@zefiris.su>
2022-07-24 03:32:55 +0300

    Fixed typo

--- src/forms/user/systems/elements/methods/SmsMethod.svelte
+++ src/forms/user/systems/elements/methods/SmsMethod.svelte
@@ -14,11 +14,11 @@
 
 <div class="spacy-below flex" transition:slide>
     <MethodDetail name="Способ">Отправка СМС-сообщения</MethodDetail>
-    <MethodDetail name="Номер"><a href={link}>{method.number}</MethodDetail>
+    <MethodDetail name="Номер"><a href={link}>{method.number}</a></MethodDetail>
     <MethodDetail name="Сообщение">{message}</MethodDetail>
     {#if comment !== ""}
         <MethodDetail name="Примечание">{comment}</MethodDetail>
     {/if}
     <br>
-    <MethodDetail name="Подсказка:">Нажмите на ссылку в поле "Номер" для отправки сообщения с вашего устройства, или отправьте указанное собщение на указанный номер вручную. После успешной отправки сообщения нажмите "Завершить".</MethodDetail>
+    <MethodDetail name="Подсказка">Нажмите на ссылку в поле "Номер" для отправки сообщения с вашего устройства, или отправьте указанное собщение на указанный номер вручную. После успешной отправки сообщения нажмите "Завершить".</MethodDetail>
 </div>
